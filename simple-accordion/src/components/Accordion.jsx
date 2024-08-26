1fadbe81410cf3299e173279d99bd2333ec731fc
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-27 00:48:52 +0530

    basic functionality implemented

--- simple-accordion/src/components/Accordion.jsx
+++ simple-accordion/src/components/Accordion.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react"
+import data from "./data";
 
-const Accordion = () => {
-  return (
-    <div>accordion</div>
-  )
-}
 
-export default Accordion
+const Accordian = () =>{
+    const [selected, setSelected] = useState(null);
+    
+    function selectedTitle (id){
+      setSelected(id)
+    }
+    return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? 
+          data.map( (dataItem) => 
+          (<div className="item" key= {dataItem.id}>
+            <div className="title" onClick ={() => selectedTitle(dataItem.id)}>
+              <h3>{dataItem.question}</h3>
+              
+                {selected === dataItem.id ?  
+                <div className="content">
+                  {dataItem.answer}
+                </div>: <span>+</span>
+
+              }
+
+            </div>
+          </div>))
+        :
+          <div>
+            No data found
+          </div>
+        }
+      </div>
+          
+    </div>)
+
+} 
+
+export default Accordian
---
+++ simple-accordion/src/components/data.js
@@ -0,0 +1,22 @@
+const data = [ {
+    id: '1',
+    question: " Why doesn't DJ Khaled ever play hide and seek",
+    answer : " Because all he does is shout, “Another one!” and you know exactly where he is!"
+} , {
+    id: '2',
+    question: "Why did Kanye bring a ladder to the studio?",
+    answer : "Because he heard his ego was on another level!"
+}
+, {
+    id: '3',
+    question: "Why can't you trust an atom?",
+    answer : " Because they make up everything!"
+}
+, {
+    id: '4',
+    question: "Why was the math book sad at the science party?",
+    answer : " Because it had too many problems!"
+}
+]
+
+export default data
