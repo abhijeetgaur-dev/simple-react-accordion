368a080a83dcda46e167992d3abbf90b3145e25a
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-27 20:19:51 +0530

    new update

--- simple-accordion/src/components/Accordion.jsx
+++ simple-accordion/src/components/Accordion.jsx
@@ -1,40 +1,49 @@
-import { useState } from "react"
+import { useState } from "react";
 import data from "./data";
+import "./Accordion.css";
 
+const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiSelected, SetMultiSelected] = useState(null);
 
-const Accordian = () =>{
-    const [selected, setSelected] = useState(null);
-    
-    function selectedTitle (id){
-      setSelected(id)
-    }
-    return (
+  function selectedTitle(id) {
+    selected === id ? setSelected(null) : setSelected(id);
+  }
+  return (
     <div className="wrapper">
-      <div className="accordian">
-        {data && data.length > 0 ? 
-          data.map( (dataItem) => 
-          (<div className="item" key= {dataItem.id}>
-            <div className="title" onClick ={() => selectedTitle(dataItem.id)}>
-              <h3>{dataItem.question}</h3>
-              
-                {selected === dataItem.id ?  
-                <div className="content">
-                  {dataItem.answer}
-                </div>: <span>+</span>
-
-              }
-
+      <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+        </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+              <div className="title"
+                   onClick={enableMultiSelection
+                    ? () => enableMultiSelection(dataItem.id)
+                    : () => selectedTitle(dataItem.id)
+                   }
+                   >
+                <h3>{dataItem.question}</h3>
+                {selected === dataItem.id ? (
+                  <div className="expand-element">-</div>
+                ) : (
+                  <div className="expand-element">+</div>
+                )}
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
-          </div>))
-        :
-          <div>
-            No data found
-          </div>
-        }
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
       </div>
-          
-    </div>)
+    </div>
+  );
+};
 
-} 
-
-export default Accordian
+export default Accordion;
--- simple-accordion/src/main.jsx
+++ simple-accordion/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Accordion from "./components/Accordion"
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Accordion/>
