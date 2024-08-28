9dfa8b02cf195c39abcd54e98212b57594c2c8c5
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-28 23:34:56 +0530

    Added multiSelection feature

--- simple-accordion/src/components/Accordion.jsx
+++ simple-accordion/src/components/Accordion.jsx
@@ -4,28 +4,43 @@ import "./Accordion.css";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+  const [multiSelected, SetMultiSelected] = useState([]);
   const [enableMultiSelection, setEnableMultiSelection] = useState(false);
-  const [multiSelected, SetMultiSelected] = useState(null);
 
   function selectedTitle(id) {
     selected === id ? setSelected(null) : setSelected(id);
   }
+
+  function multipleSelectedTitle(id) {
+    let cpyMultiSelected = [...multiSelected];
+    const findIndexOfCurrentId = cpyMultiSelected.indexOf(id);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiSelected.push(id);
+    else cpyMultiSelected.splice(findIndexOfCurrentId, 1);
+
+    SetMultiSelected(cpyMultiSelected);
+  }
+
+  console.log(selected, multiSelected);
   return (
     <div className="wrapper">
-      <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)}>
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
         Enable Multi Selection
-        </button>
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item" key={dataItem.id}>
               {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-              <div className="title"
-                   onClick={enableMultiSelection
-                    ? () => enableMultiSelection(dataItem.id)
+              <div
+                className="title"
+                onClick={
+                  enableMultiSelection
+                    ? () => multipleSelectedTitle(dataItem.id)
                     : () => selectedTitle(dataItem.id)
-                   }
-                   >
+                }
+              >
                 <h3>{dataItem.question}</h3>
                 {selected === dataItem.id ? (
                   <div className="expand-element">-</div>
@@ -33,7 +48,8 @@ const Accordion = () => {
                   <div className="expand-element">+</div>
                 )}
               </div>
-              {selected === dataItem.id ? (
+              {selected === dataItem.id ||
+              multiSelected.indexOf(dataItem.id) !== -1 ? (
                 <div className="content">{dataItem.answer}</div>
               ) : null}
             </div>
