1ac81d4a19126453927b1a22de70bda4a4a9c189
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-09-20 17:55:30 +0530

    react practice

--- simple-accordion/src/components/Accordion.jsx
+++ simple-accordion/src/components/Accordion.jsx
@@ -1,65 +1,28 @@
-import { useState } from "react";
-import data from "./data";
-import "./Accordion.css";
+import { useState } from "react"
+import data from "./data"
 
-const Accordion = () => {
-  const [selected, setSelected] = useState(null);
-  const [multiSelected, SetMultiSelected] = useState([]);
-  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
-
-  function selectedTitle(id) {
-    selected === id ? setSelected(null) : setSelected(id);
+const Accordion = () =>{
+  let [selected, setSelected] = useState(0)
+  const getCurrentId = (id) =>{
+    console.log(id)
+    selected === id ? setSelected(null) : setSelected(id)
   }
-
-  function multipleSelectedTitle(id) {
-    let cpyMultiSelected = [...multiSelected];
-    const findIndexOfCurrentId = cpyMultiSelected.indexOf(id);
-
-    console.log(findIndexOfCurrentId);
-    if (findIndexOfCurrentId === -1) cpyMultiSelected.push(id);
-    else cpyMultiSelected.splice(findIndexOfCurrentId, 1);
-
-    SetMultiSelected(cpyMultiSelected);
-  }
-
-  console.log(selected, multiSelected);
   return (
-    <div className="wrapper">
-      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
-        Enable Multi Selection
-      </button>
-      <div className="accordion">
-        {data && data.length > 0 ? (
-          data.map((dataItem) => (
-            <div className="item" key={dataItem.id}>
-              {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-              <div
-                className="title"
-                onClick={
-                  enableMultiSelection
-                    ? () => multipleSelectedTitle(dataItem.id)
-                    : () => selectedTitle(dataItem.id)
-                }
-              >
-                <h3>{dataItem.question}</h3>
-                {selected === dataItem.id ? (
-                  <div className="expand-element">-</div>
-                ) : (
-                  <div className="expand-element">+</div>
-                )}
-              </div>
-              {selected === dataItem.id ||
-              multiSelected.indexOf(dataItem.id) !== -1 ? (
-                <div className="content">{dataItem.answer}</div>
-              ) : null}
-            </div>
-          ))
-        ) : (
-          <div>No data found</div>
-        )}
-      </div>
-    </div>
-  );
-};
+    <div>
+      {data.map((dataItem) => (
+        <div key= {dataItem.id}>
+          <div className= "post-question" onClick= {() => getCurrentId(dataItem.id)}>{dataItem.question}</div>
+          {selected === dataItem.id && 
+          <div className = "post-answer">{dataItem.answer}</div>
+          }
+          
+        </div>
+      ))}
 
-export default Accordion;
+    </div>
+
+  )
+}
+
+
+export default Accordion
