089fd45871ed3526be7c6e288ff3c6bea30ce5b3
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-09-21 17:10:16 +0530

    accordion complete

--- simple-accordion/src/components/Accordion.jsx
+++ simple-accordion/src/components/Accordion.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react"
 import data from "./data"
 
 const Accordion = () =>{
-  const getCurrentId = (id) =>{
+  let [selected, setSelected] = useState(0)
+  let [multiSelected, setMultiSelected] = useState([])
+  
+  let [enableMultiSelect, setEnableMultiSelect] = useState(0)
+
+  const multipleSelect = () =>{
+    console.log(enableMultiSelect)
+    enableMultiSelect ? setEnableMultiSelect(0) : setEnableMultiSelect(1)
+  }
+  
+  const multipleGetCurrentId = (id) =>{
+    let cpyMultiple = [...multiSelected]
+    let indexOfCurrentId = cpyMultiple.indexOf(id)
+
+    cpyMultiple.indexOf(id) == -1 ? cpyMultiple.push(id) : cpyMultiple.splice(indexOfCurrentId , 1)
+
+    setMultiSelected(cpyMultiple)
+    console.log(cpyMultiple)
+  }
+  
+  const singleGetCurrentId = (id) =>{
     console.log(id)
+    selected ===id ? setSelected(0) : setSelected(id) 
+
   }
 
   return (
     <div className="wrapper">
+      <button onClick = {multipleSelect}>Enable Multi Select</button>
       {data.map((dataItem) => (
         <div key={dataItem.id} className="post"> 
-          <div className="post-question" onClick={() => getCurrentId(dataItem.id)}><h1>{dataItem.question}</h1></div>
-          <div className="post-answer">{dataItem.answer}</div>
+          <div 
+          className="post-question"
+          onClick={() => enableMultiSelect ?  multipleGetCurrentId(dataItem.id) :singleGetCurrentId(dataItem.id)}>
+          <h3>{dataItem.question}</h3></div>
+          {enableMultiSelect
+              ? multiSelected.indexOf(dataItem.id) !== -1 &&   <div className="post-answer">{dataItem.answer}</div> 
+              : selected === dataItem.id &&  <div className="post-answer">{dataItem.answer}</div> 
+          }
         </div>
       ))}
     </div>
--- simple-accordion/src/main.jsx
+++ simple-accordion/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Accordion from "./components/Accordion"
+import './components/Accordion.css'
 
 
 createRoot(document.getElementById('root')).render(
